fab52e6788f6a5c059972523103ddf98726d4e9d
Robin <robinrobix14@gmail.com>
2024-12-03 14:48:18 +0100

    add sw notif

---
+++ notifications.js
@@ -0,0 +1,10 @@
+if (Notification.permission !== 'granted') {
+    if (confirm('Accepter les notifications?')){
+        Notification.requestPermission().then( permission => {
+            console.log(permission)
+            if (permission === "granted") {
+                console.log("Notification possible")
+            }
+        })
+    }
+}
